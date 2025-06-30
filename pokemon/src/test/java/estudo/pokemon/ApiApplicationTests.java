95018b2d603ea328d13ced6ff3804baa81f2e0a6
Kaio SM <kaioangelus007@hotmail.com>
2025-06-30 11:22:11 -0300

    primeiro commit.

---
+++ pokemon/src/test/java/estudo/pokemon/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
